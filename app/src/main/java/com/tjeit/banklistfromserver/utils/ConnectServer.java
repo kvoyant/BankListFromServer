fe42499646bebdbf95a208d01968534154a147a7
yhkim <kvoyant@gmail.com>
2019-05-11 15:44:07 +0900

    은행정보 API호출 예시 -GET바방식, 파라미터 없는 경우

--- app/src/main/java/com/tjeit/banklistfromserver/utils/ConnectServer.java
+++ app/src/main/java/com/tjeit/banklistfromserver/utils/ConnectServer.java
@@ -1,8 +1,18 @@
 package com.tjeit.banklistfromserver.utils;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ConnectServer {
     //서버의 근본 주소
@@ -15,6 +25,36 @@ public class ConnectServer {
 
     //필요한 서버의 요청들을 하나하나 메쏘드로 만들어 주자.
     public static void getRequestInfoBank(Context context, /* 필요한 파라미터 변수들*/ final JsonResponsHandler handler) {
+
+        //      서버 - 클라이언트(앱)
+        OkHttpClient client = new OkHttpClient();
+
+//        URL 설정 => 목적지 설정
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL + "/info/bank").newBuilder();
+
+//        ※ GET, DELETE 메쏘드는 필요 파라미터를 URL에 담아 줘야 함. 이 담는 과정을 쉽게 하려고 urlBuilder 사용.
+
+//        실제로 서버에 접근하는 완성된 URL
+        String requestURL = urlBuilder.build().toString();
+
+//        완성된 URL로 접근하는 request 생성.
+        Request request = new Request.Builder().url(requestURL).build();
+
+//        만들어진 Request를 실제로 서버에 요청
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String reponseContent = response.body().string();
+
+                Log.d("서버 응답 내용", reponseContent);
+            }
+        });
 
     }
 
