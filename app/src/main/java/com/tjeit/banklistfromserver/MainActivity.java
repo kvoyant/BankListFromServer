df913623d16e8916a2ff860c43906faed1c0f6cf
yhkim <kvoyant@gmail.com>
2019-05-12 11:57:50 +0900

    자동 새로고침 - 안드로이드 생명주기 - onResume 활용테스트 -list 클리어 기능(중복 데이터 제거)

--- app/src/main/java/com/tjeit/banklistfromserver/MainActivity.java
+++ app/src/main/java/com/tjeit/banklistfromserver/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tjeit.banklistfromserver;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -38,60 +39,7 @@ public class MainActivity extends BaseActivity {
         act.serverTestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponsHandler() {
-                    @Override
-                    public void onResponse(JSONObject json) {
-//                      실제로 서보에서 돌아온 응답을 메인 액티비티에서 처리하는 메쏘드.
 
-                        try {
-                            int code = json.getInt("code");
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if (code == 200) {
-                                        Toast.makeText(mContext, "정상적으로 가져왔습니다.", Toast.LENGTH_SHORT).show();
-
-                                        try {
-                                            JSONObject data = json.getJSONObject("data");
-                                            JSONArray banks = data.getJSONArray("banks");
-
-                                            for(int i=0;i<banks.length();i++) {
-                                                JSONObject bank = banks.getJSONObject(i);
-
-//                                                String name = bank.getString("name");
-//                                                Log.d("은행이름", name);
-//                                                int id = bank.getInt("id");
-//                                                String code = bank.getString("code");
-//                                                String logo = bank.getString("logo");
-
-                                                Bank bankObj = Bank.getBankFromJson(bank);
-                                                bankList.add(bankObj);
-
-                                            }
-                                            bankAdapter.notifyDataSetChanged();
-
-                                        } catch (JSONException e) {
-                                            e.printStackTrace();
-                                        }
-                                    } else {
-//                                        서버에서 주는 에러메세지를 토스트로 출력
-                                        try {
-                                            String message = json.getString("message");
-                                            Toast.makeText(mContext, message,  Toast.LENGTH_SHORT).show();
-                                        } catch (JSONException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }
-                            });
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                });
             }
         });
     }
@@ -100,6 +48,66 @@ public class MainActivity extends BaseActivity {
     public void setValues() {
         bankAdapter = new BankAdapter(mContext, bankList);
         act.bankListView.setAdapter(bankAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponsHandler() {
+            @Override
+            public void onResponse(JSONObject json) {
+//                      실제로 서보에서 돌아온 응답을 메인 액티비티에서 처리하는 메쏘드.
+
+                try {
+                    int code = json.getInt("code");
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (code == 200) {
+                                Toast.makeText(mContext, "정상적으로 가져왔습니다.", Toast.LENGTH_SHORT).show();
+
+                                try {
+                                    JSONObject data = json.getJSONObject("data");
+                                    JSONArray banks = data.getJSONArray("banks");
+
+//                                          누적으로 데이터가 쌓이지 않게
+                                    bankList.clear();
+
+                                    for(int i=0;i<banks.length();i++) {
+                                        JSONObject bank = banks.getJSONObject(i);
+
+//                                                String name = bank.getString("name");
+//                                                Log.d("은행이름", name);
+
+
+                                        Bank bankObj = Bank.getBankFromJson(bank);
+                                        bankList.add(bankObj);
+
+                                    }
+                                    bankAdapter.notifyDataSetChanged();
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+//                                        서버에서 주는 에러메세지를 토스트로 출력
+                                try {
+                                    String message = json.getString("message");
+                                    Toast.makeText(mContext, message,  Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
     @Override
