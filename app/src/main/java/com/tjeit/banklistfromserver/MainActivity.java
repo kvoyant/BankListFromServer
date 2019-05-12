6bab82502da69559b35948118651060f24d0fb3e
yhkim <kvoyant@gmail.com>
2019-05-12 10:42:48 +0900

    JSON파싱기초 =>토스트화면 출력

--- app/src/main/java/com/tjeit/banklistfromserver/MainActivity.java
+++ app/src/main/java/com/tjeit/banklistfromserver/MainActivity.java
@@ -4,9 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.tjeit.banklistfromserver.databinding.ActivityMainBinding;
 import com.tjeit.banklistfromserver.utils.ConnectServer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
     ActivityMainBinding act;
@@ -24,7 +28,37 @@ public class MainActivity extends BaseActivity {
         act.serverTestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConnectServer.getRequestInfoBank(mContext,null);
+                ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponsHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+//                      실제로 서보에서 돌아온 응답을 메인 액티비티에서 처리하는 메쏘드.
+
+                        try {
+                            int code = json.getInt("code");
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (code == 200) {
+                                        Toast.makeText(mContext, "정상적으로 가져왔습니다.", Toast.LENGTH_SHORT).show();
+                                    } else {
+//                                        서버에서 주는 에러메세지를 토스트로 출력
+                                        try {
+                                            String message = json.getString("message");
+                                            Toast.makeText(mContext, "서버통신에 문제가 있습니다.", Toast.LENGTH_SHORT).show();
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            });
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
             }
         });
     }
